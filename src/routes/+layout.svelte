a710e52c2db13b45d9d51e45da3bf88b20f4ad88
Ian Shakespeare <irshakes11@gmail.com>
2023-06-24 20:05:33 -0600

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import "../app.css";
+</script>
+
+<main
+  class="max-w-6xl mx-auto h-full border p-4 bg-white border-slate-200 dark:bg-slate-900 dark:border-slate-700"
+>
+  <slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  const itinerary = [
+    ["Sun, March 10", "Arrive in Tokyo"],
+    ["Mon, March 11", "Ian busy all day"],
+    ["Tue, March 12", "Asakusa, Tokyo Sky Tree"],
+    ["Wed, March 13", "Tokyo DisneySea"],
+    ["Thu, March 14", "Meiji-Jingu, Shibuya"],
+    ["Fri, March 15", ""],
+    ["Sat, March 16", "Wedding in Nagoya"],
+    ["Sun, March 17", ""],
+  ];
+</script>
+
+<svelte:head>
+  <title>Wedding Trip Itinerary</title>
+</svelte:head>
+<section class="grid gap-2">
+  <h1 class="text-3xl font-semibold text-center">Wedding Trip Itinerary</h1>
+  <p class="text-center">
+    The following is a list of all activities we plan on doing. Guests are
+    welcome to split off and find their own activities.
+  </p>
+  <table>
+    <tr class="text-left font-semibold">
+      <th class="p-2">Date</th>
+      <th class="p-2">Event</th>
+    </tr>
+    {#each itinerary as [date, event]}
+      <tr class="rounded even:bg-slate-100 even:dark:bg-slate-800">
+        <td class="p-2">{date}</td>
+        <td class="p-2">{event}</td>
+      </tr>
+    {/each}
+  </table>
+</section>
